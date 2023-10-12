5fa24ca89a2b8aacfec18b673d7ee3979fa05724
thachha901 <124699333+thachha901@users.noreply.github.com>
2023-10-12 13:38:44 +0700

    Update Main.java

--- src/main/java/com/example/demo1/Main.java
+++ src/main/java/com/example/demo1/Main.java
@@ -1,10 +1,9 @@
 package com.example.demo1;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
